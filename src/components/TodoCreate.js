b368bf444420555069eb01db327a966e2b87ef48
Changhyeon Baek <sabondev@gmail.com>
2025-04-04 15:57:51 +0900

    Fix undefined 'add' by importing from todoSlice

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
 import { MdAdd } from 'react-icons/md'
 import { useDispatch } from 'react-redux'
+import { add } from '../store/todoSlice';
 
 const CreateButton = styled.button`
   background: #00704a;
